6876333253ee21ec91a127b512dff4147a3f9952
SAID Ismael <ismael.saidpro@protonmail.com>
2023-09-13 19:21:54 +0200

    Update : Example of component use in a browser

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
+import { GithubModule } from './github/github.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    GithubModule
   ],
   providers: [],
   bootstrap: [AppComponent]

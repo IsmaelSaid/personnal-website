b5cdf04dd0052dfcd5d3850cd5f9d5ab72b0477d
SAID Ismael <ismael.saidpro@protonmail.com>
2023-09-19 14:56:44 +0200

    New : Adding a navigation bar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,16 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { GithubModule } from './github/github.module';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { UiModule } from './ui/ui.module';
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    GithubModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, GithubModule, BrowserAnimationsModule, UiModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}

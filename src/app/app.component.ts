a10131e7d0da45de1e400af12e6ea4427463188e
SAID Ismael <ismael.saidpro@protonmail.com>
2023-09-23 20:35:03 +0200

    New : Component to display information about a github user (Landing page)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { GithubUserInfos } from './github/models/github-user';
+import { Observable } from 'rxjs';
+import { GithubdataService } from './github/services/github-data.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  constructor(private githubdataservice: GithubdataService) {}
   title = 'site-perso';
+  githubUser$!: Observable<GithubUserInfos>;
+  ngOnInit(): void {
+    this.githubUser$ = this.githubdataservice.getUser('IsmaelSaid');
+  }
 }
--- src/app/ui/ui.module.ts
+++ src/app/ui/ui.module.ts
@@ -3,10 +3,12 @@ import { CommonModule } from '@angular/common';
 import { NavbarComponent } from './navbar/navbar.component';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
+import { LandingPageComponent } from './landing-page/landing-page.component';
+import { MatGridListModule } from '@angular/material/grid-list';
 
 @NgModule({
-  declarations: [NavbarComponent],
-  imports: [CommonModule, MatToolbarModule, MatButtonModule],
-  exports: [NavbarComponent],
+  declarations: [NavbarComponent, LandingPageComponent],
+  imports: [CommonModule, MatToolbarModule, MatButtonModule, MatGridListModule],
+  exports: [NavbarComponent, LandingPageComponent, MatGridListModule],
 })
 export class UiModule {}
